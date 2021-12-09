d5e6275198eefdf86b60eb2ee3ca80ea0f02de2e
Thibaut1304 <thibaut.husser@free.fr>
2021-12-09 12:44:09 +0100

    cmd exit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:25:01 by thhusser          #+#    #+#             */
-/*   Updated: 2021/12/09 12:32:37 by thhusser         ###   ########.fr       */
+/*   Updated: 2021/12/09 12:40:54 by thhusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char **argv, char **env)
 	while (1)
 	{
 		ft_putstr(_GREEN"thhusser> "_NC);
-		if (!get_next_line(0, &line))
+		if (!get_next_line(0, &line) || !ft_strcmp(line, "exit"))
 			ft_exit(1, line);
 		ft_del_line(line);
 	}
