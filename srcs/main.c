2dfb01f622e79bd7fc7b8294a437f0d828234f5d
Thibaut1304 <thibaut.husser@free.fr>
2021-12-13 17:33:23 +0100

    raisonment trouve et analyse parseur

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 16:25:01 by thhusser          #+#    #+#             */
-/*   Updated: 2021/12/13 13:08:30 by thhusser         ###   ########.fr       */
+/*   Updated: 2021/12/13 17:26:58 by thhusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,7 @@ int		parse_line(t_ms *g)
 	// g->parsing = malloc(sizeof(t_data) * nb_cmd);
 	// if (!g->parsing)
 		// ft_exit(0, g);
+	printf(_GREEN"%s\n"_NC, g->line);
 	while (tmp)
 	{
 		x = -1;
@@ -72,22 +73,21 @@ int		parse_line(t_ms *g)
 		line = ft_strdup(tmp->content);
 		// line_split = ft_split_charset(line, " \t");
 		printf(_RED"%s\n"_NC, line);
-		printf(_GREEN"%s\n"_NC, g->line);
 		// while (line_split[++x])
-		// {
-		// 	option = check_option(line_split[x]);
-		// 	if (option)
-		// 	{
-		// 		tmp_line = ft_strjoin(line_split[x], " ");
-		// 		record_list(&g->cmd, ft_strjoin(tmp_line, option));
-		// 		ft_del_line(tmp_line);
-		// 	}
-		// 	ft_del_line(line_split[x]);
-		// 	ft_del_line(option);
-		// }
+		{
+			// option = check_option(line_split[x]);
+			// if (option)
+			// {
+			// 	tmp_line = ft_strjoin(line_split[x], " ");
+			// 	record_list(&g->cmd, ft_strjoin(tmp_line, option));
+			// 	ft_del_line(tmp_line);
+	 		// 	ft_del_line(option);
+			// }
+			// ft_del_line(line_split[x]);
+		}
 		// free(line_split);
+		// line_split = NULL;
 		ft_del_line(line);
-		line_split = NULL;
 		tmp = tmp->next;
 	}
 	ft_lstclear(&g->cmd_tmp, &ft_del_list);
@@ -124,6 +124,63 @@ char	*get_cmd_in_line_th(char *line, t_ms *g)
 	return (line);
 }
 
+int		parseur(t_ms *g, int i, int res)
+{
+	while (g->line[++i])
+	{
+		//check double cote ou simple cote
+		if (g->line[i] == '\'' || g->line[i] == '"') // les passer avec les fonctions et retourner i pour decaller l'index
+		{
+			i = parseur_quotes(i + 1, g->line[i]);
+			if (i == -1)
+				return ();//generer une erreur correspondate a bash
+		}
+		// checek chevron in et out
+		if (g->line[i] == '>')
+		{
+			res = parsing_redirection_out(i);
+			if (res != 0)
+				return (res);
+		}
+		if (g->line[i] == '<')
+		{
+			res = parsing_redirection_in(i);
+			if (res != 0)
+				return (res);
+		}
+		// check pipe
+		if (g->line[i] == '|')
+		{
+			res = parsing_pipe(i);
+			if (res != 0)
+				return (res);
+		}
+		//si back slash i++
+		if (g->line[i] == '\\')
+			i++;
+		//efectuer les fonction de test et return res pour les erreurs
+		//chercher ou mettre les commande $ ... pour echo !
+	}
+	return(0);
+}
+
+void	clean_command(t_ms *g)
+{
+	int	i;
+	int	pipe;
+
+	i = -1;
+	pipe = 0;
+	if (parseur(g, -1, 0))
+		return (1);
+	// parseur va check tous les padding probleme de cote ...
+	// ensuite enlever tous les espace en debut de ligne
+	// ensuite test sur la commande si pipe sinon commande a executer
+	// --> ou alors boucle pour le nombre de pipe present dans la commande
+
+
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*cmd;
@@ -140,18 +197,19 @@ int	main(int argc, char **argv, char **env)
 		ft_putstr(_GREEN"thhusser> "_NC);
 		if (!get_next_line(0, &g.line) || !ft_strcmp(g.line, "exit"))
 			ft_exit(2, &g);
-		cmd = get_cmd_in_line_th(g.line, &g);
-		if (first_command(&g))
-			ft_exit(0, &g);
-		// print_list(g.cmd_tmp);
-		if (!find_cmd_path(cmd, &g))
-		{
-			ft_putstr("minishell: ");
-			ft_putstr(cmd);
-			ft_putstr(": command not found\n");
-		}
-		if (!ft_strcmp(g.line, "env"))
-			print_list(g.env);
+		// cmd = get_cmd_in_line_th(g.line, &g);
+		// if (first_command(&g))
+		// 	ft_exit(0, &g);
+		// // print_list(g.cmd_tmp);
+		// if (!find_cmd_path(cmd, &g))
+		// {
+		// 	ft_putstr("minishell: ");
+		// 	ft_putstr(cmd);
+		// 	ft_putstr(": command not found\n");
+		// }
+		// if (!ft_strcmp(g.line, "env"))
+		// 	print_list(g.env);
+		clean_command(&g);
 		ft_del_line(g.line);
 	}
 	return (0);
