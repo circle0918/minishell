c342ca738ae2c4e332f04a1b506267cf9dad8a48
thibaut1304 <thibaut.husser@free.fr>
2022-01-16 06:22:13 +0100

    free history --> read line leaks a mort

--- srcs/exit_free.c
+++ srcs/exit_free.c
@@ -6,7 +6,7 @@
 /*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 10:33:33 by thhusser          #+#    #+#             */
-/*   Updated: 2022/01/16 04:40:49 by thhusser         ###   ########.fr       */
+/*   Updated: 2022/01/16 06:10:24 by thhusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	ft_exit(int nb, t_ms *g, int ret, char *line)
 	{
 		if (nb == 2)
 			ft_putstr("exit\n");
+		rl_clear_history();
 		ft_del_line(g->line); //--> re check si a enlever !
 		ft_del_line(g->ret_dir);
 		ft_lstclear(&g->env, &ft_del_list);
