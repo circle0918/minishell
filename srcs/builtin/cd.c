7544306e0a6cf8688aff929167b3d1d47b9609b1
user42 <yyuan@student.42.fr>
2022-01-19 01:08:41 +0100

    norm cd echo pwd unset

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -28,32 +28,32 @@ char	*get_env(char *str, t_list *env)
 
 	tmp = env;
 	pos = NULL;
-	while(tmp)
+	while (tmp)
 	{
 		pos = ft_strstr(tmp->content, str);
 		if (pos && pos - (char *)tmp->content == 0)
 		{
-			return pos + ft_strlen(str) + 1;
+			return (pos + ft_strlen(str) + 1);
 		}
 		tmp = tmp->next;
 	}
-	return NULL;
+	return (NULL);
 }
 
-void set_env(char *key, char *val, t_list *env)
+void	set_env(char *key, char *val, t_list *env)
 {
-	t_list *tmp;
-	char* pos;
-	char s[1024];
-	t_list *last;
+	t_list	*tmp;
+	char	*pos;
+	char	s[1024];
+	t_list	*last;
 
 	s[0] = '\0';
 	ft_strcat(s, key);
 	ft_strcat(s, "=");
-    ft_strcat(s, val);
+	ft_strcat(s, val);
 	tmp = env;
 	pos = NULL;
-	while(tmp)
+	while (tmp)
 	{
 		pos = ft_strstr(tmp->content, key);
 		if (pos && pos - (char *)tmp->content == 0)
@@ -64,18 +64,16 @@ void set_env(char *key, char *val, t_list *env)
 		}
 		tmp = tmp->next;
 	}
-	//append
-	//record the new before the last(_=./minishell)
 	last = ft_lst_pop_last(&env);
 	record_list(&env, s);
 	ft_lstadd_back(&env, last);
 }
 
-void change_path(char *path, t_ms *g, int change_back)
+void	change_path(char *path, t_ms *g, int change_back)
 {
-	char* pwd;
-	path = get_pwd();
+	char	*pwd;
 
+	path = get_pwd();
 	if (change_back)
 	{
 		pwd = ft_strdup(get_env("PWD", g->env));
@@ -91,29 +89,28 @@ void change_path(char *path, t_ms *g, int change_back)
 	}
 }
 
-void ft_cd(t_ms *g)
+void	ft_cd(t_ms *g)
 {
-	char *path;
-	char cwd[1024];
-	char *tmp_path;
-	if(g->cmd_ac == 2)
+	char	*path;
+	char	cwd[1024];
+	char	*tmp_path;
+
+	if (g->cmd_ac == 2)
 		path = ft_strdup(g->cmd_tab[1]);
 	g->ret_errno = 0;
-	if(g->cmd_ac > 2)
+	if (g->cmd_ac > 2)
 	{
 		g->ret_errno = 1;
 		error_out2("cd", path, "Too many arguements");
 	}
-	if (g->cmd_ac == 1|| ft_strequ(path, "~") || ft_strequ(path, "--"))
+	if (g->cmd_ac == 1 || ft_strequ(path, "~") || ft_strequ(path, "--"))
 	{
-		//path == home
 		if (g->cmd_ac == 2)
 			free(path);
 		path = get_env("HOME", g->env);
 	}
 	if (ft_strequ(path, "-"))
 	{
-		//path == OLDPWD
 		free(path);
 		path = get_env("OLDPWD", g->env);
 		printf("cd - :%s\n", path);
@@ -127,19 +124,18 @@ void ft_cd(t_ms *g)
 		}
 		return ;
 	}
-	if(path[0] == '~')
+	if (path[0] == '~')
 	{
-		//path: replace ~ to home
 		tmp_path = path;
 		path = ft_strjoin(get_env("HOME", g->env), (tmp_path + 1));
 		free(tmp_path);
 	}
-    if (path[0] != '/')
-    {// true for the dir in cwd
-        getcwd(cwd, sizeof(cwd));
-        ft_strcat(cwd, "/");
-        ft_strcat(cwd, path);
-        if (chdir(cwd) == 0)
+	if (path[0] != '/')
+	{
+		getcwd(cwd, sizeof (cwd));
+		ft_strcat(cwd, "/");
+		ft_strcat(cwd, path);
+		if (chdir(cwd) == 0)
 			change_path(cwd, g, 0);
 		else
 		{
@@ -147,10 +143,10 @@ void ft_cd(t_ms *g)
 			error_out2("cd", path, "No such file or directory");
 			g->ret_errno = 1;
 		}
-    }
+	}
 	else
 	{
-        if (chdir(path) == 0)
+		if (chdir(path) == 0)
 			change_path(path, g, 0);
 		else
 		{
--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -1,21 +1,20 @@
 #include "../includes/minishell.h"
 #include <string.h>
-int		check_n(char **tab)
-{
-	int i;
-	int j;
 
-	//echo hello -n ==> hello -n
-	//echo -n hello == echo -nnn hello === echo -n -nnnn hello ==> hello without '\n'
+int	check_n(char **tab)
+{
+	int	i;
+	int	j;
+
 	i = 1;
-	while(tab[i])
+	while (tab[i])
 	{
 		if (tab[i][0] != '-')
-			break;
+			break ;
 		if (!tab[i][1] || tab[i][1] != 'n')
-			break;
+			break ;
 		j = 2;
-		while(tab[i][j])
+		while (tab[i][j])
 		{
 			if (tab[i][j] != 'n')
 				return (i);
@@ -25,46 +24,24 @@ int		check_n(char **tab)
 	}
 	return (i);
 }
-/*
-void echo_printf(char *s)
-{
-	int is_escape;
-	int is_sq;//single quote
-	int is_dq;//double quote
-	char *p;
-	char *begin;
-	char *ss;
 
-	p = s;
-	while(p)
-	{
-		if (*p == ''')
-		{
-		}
-		else if (*p == '"')
-		{
-		}
-		else
-		p++;
-	}
-}
-*/
-void ft_echo(t_ms *g)
+void	ft_echo(t_ms *g)
 {
-	int i;
-	int is_opt_n;
+	int	i;
+	int	is_opt_n;
 
 	g->ret_errno = 0;
-	//tab = ft_split(cmd, ' ');
-	if(g->cmd_ac == 1)
+	if (g->cmd_ac == 1)
 	{
 		printf("\n");
-		//free_split(tab);
-		return;
+		return ;
 	}
 	i = check_n(g->cmd_tab);
-	is_opt_n = i > 1 ? 1 : 0;
-	while(g->cmd_tab[i])
+	if (i > 1)
+		is_opt_n = 1;
+	else
+		is_opt_n = 0;
+	while (g->cmd_tab[i])
 	{
 		printf("%s", g->cmd_tab[i]);
 		i++;
@@ -73,5 +50,4 @@ void ft_echo(t_ms *g)
 	}
 	if (!is_opt_n)
 		printf("\n");
-//	free_split(tab);
 }
