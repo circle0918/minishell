2e1cdfaa1ac57de3e117cbc9f45ae570185e06de
Thibaut1304 <thibaut.husser@free.fr>
2022-01-19 16:25:42 +0100

    fix double free

--- srcs/variables_1.c
+++ srcs/variables_1.c
@@ -6,7 +6,7 @@
 /*   By: thhusser <thhusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 20:29:50 by thhusser          #+#    #+#             */
-/*   Updated: 2022/01/19 02:07:31 by thhusser         ###   ########.fr       */
+/*   Updated: 2022/01/19 16:25:05 by thhusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,6 @@ char	*get_str_var(char *str, char *tmp, int idx, int sp)
 		sp++;
 	if (str[sp] != '\0')
 		ft_strcat(tmp, &str[idx]);
-	ft_del_line(str);
 	str = (char *)ft_calloc(sizeof(char), (ft_strlen(tmp) + 1));
 	if (!str)
 		return (NULL);
